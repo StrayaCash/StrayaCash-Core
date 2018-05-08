9b9ee893c48ee03fe1831791c00442c918c3b253
AdelaideCreative <david@adelaidecreative.com.au>
2018-05-08 15:50:10 +0930

    Updated Url

--- src/config/strayacash-config.h
+++ src/config/strayacash-config.h
@@ -268,7 +268,7 @@
 #define LT_OBJDIR ".libs/"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "www.strayacash.co"
+#define PACKAGE_BUGREPORT "www.strayacash.org" //Updated URL
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "strayacash Core"
